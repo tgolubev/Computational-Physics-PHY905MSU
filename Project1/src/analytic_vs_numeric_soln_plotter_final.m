3aa8ad9e24b4767ace38785020f48d7c5cefc7bc
tgolubev <tg8v3@mst.edu>
2017-02-06 00:32:06 -0500

    Add files

---
+++ Project1/src/analytic_vs_numeric_soln_plotter_final.m
@@ -0,0 +1,60 @@
+%Numerical vs. analytic result plotter for project1
+
+clear                                                       %to clear variables
+[File,Path]=uigetfile('*.txt','MultiSelect','on');
+
+N = numel(File);                                               %This counts the number of files that are in the cell array "File" that was outputted by uigetfile. 
+%numeric_soln = zeros(1,N);                %initialize arrays to store the numeric and analytic solutions for each file. This initialization is not necessary but makes code faster.
+%analytic_soln = zeros(1,N);
+
+for  num=1:N                                                  %Repeat loop for each file (each single curve analyzed seperately)
+
+   name= File(1,num);
+   str=sprintf('%s', [Path name{1}]);                         %makes str be the name of file (along with its path)
+   format shortG                                              %change formating so doesn't show 0's for e-11 values. 
+   data= load (str);                                          %load the .txt file into matrix called "data"
+    
+   x_data = data(:,1); 
+   num_pts = size(x_data,1);
+   
+   numeric_soln = data(:,2);
+   analytic_soln = data(:,3); 
+
+x_data_shifted = zeros(num_pts+2,1);
+x_data_shifted(2:end-1) = x_data(1:end);     %reshift the x_data by down 1 to be able to add a x=o as 1st value
+x_data_shifted(1,1) = 0;                   %add endpoints
+x_data_shifted(end,1) = 1;
+
+
+ if num==N                                                   %Only need to plot analytic solution once, and do it for the last file (this will be file w/ greatest # of pts since files are labeled w/ numbers
+                                                             %of increasing iteration/n value that was used.                          
+    analytic_soln_shifted = zeros(num_pts+2,1);           %do the reshifting only within this loop because only need analytic soln. once
+    analytic_soln_shifted(2:end-1) = analytic_soln(1:end);     %reshift the data by down 1 to be able to add a x=o as 1st value
+    analytic_soln_shifted(1,1) = 0;                             %add endpoints
+    analytic_soln_shifted(end,1) = 0;   
+     h = plot(x_data, analytic_soln); 
+     set(h,'Color','b')
+     hold on
+     set(h,'LineWidth',1.5);
+   else
+ end
+   
+
+numeric_soln_shifted = zeros(num_pts+2,1);
+numeric_soln_shifted(2:end-1) = numeric_soln(1:end);     %reshift the x_data by down 1 to be able to add a x=o as 1st value
+numeric_soln_shifted(1,1) = 0;                           %boundary conditions
+numeric_soln_shifted(end,1) = 0; 
+
+ h = plot(x_data_shifted,numeric_soln_shifted);
+ 
+ set(h,'LineWidth',1.5);                              %make lines thicker
+ 
+ hold on             
+ 
+ xlabel({'x'});
+ ylabel({'u(x)'});
+ 
+end
+
+hold off               %so doesn't add curves from different runs
+   
---
+++ Project1/src/relative_error_plotter_using_avg_error.m
@@ -0,0 +1,53 @@
+%Plotter for PHY905 program1 results.
+%Can select multiple result files at once. Finds the maximum relative error
+%value for each result file (each iteration) and uses this to plot the
+%relative error as fnc. of step size.
+%For each plot line you want, need to run the code again. It has 'hold on'
+%enabled, so eachnew generated line will be added to the currently opened
+%figure.
+
+clear                                                       %to clear variables
+[File,Path]=uigetfile('*.txt','MultiSelect','on');
+
+N = numel(File);                                               %This counts the number of files that are in the cell array "File" that was outputted by uigetfile. 
+
+for  num=1:N                                                  %Repeat loop for each file (each single curve analyzed seperately)
+
+   name= File(1,num);
+   str=sprintf('%s', [Path name{1}]);                         %makes str be the name of file (along with its path)
+   format shortG                                              %change formating so doesn't show 0's for e-11 values. 
+   data= load (str);                                          %load the .txt file into matrix called "data"
+    
+
+   rel_error = data(:,4); %log was already taken in cpp program
+   avg_error(num,1) = mean(rel_error); %finds the maximum error in the column for that specific iteration and puts it into row = num(corresponding to which iteration/file its from)
+   n = size(rel_error,1)+1; %From thenumber of rows (1st dimension of rel_error: synthax is size(variable, dimension) where for 
+                            %dimension enter 1 or 2 (row or column) in the results file, add 1 to get matrix size that was used.
+                    
+   step_size(num,1) = 1/n;  %can't use h() because that's reserved for handles in Matlab
+   log_h = log10(step_size);
+   
+
+%averaging
+
+%  average = mean(log_y_data,2);
+%  
+%  figure;
+%  plot(x_data,average)
+end
+
+ set(0,'defaulttextinterpreter','tex')  %Sets .tex format as the default text interpreter. This displays i.e. 'log_{10}' with 10 being a subscript on labels. Use i.e. 'words^{superscript}'
+                                        %to make a superscript.
+
+ g = plot(log_h, avg_error);
+ set(g,'Color','b')
+ set(g,'LineWidth',1.5);                              %make lines thicker
+ hold on                                              %here we purposely leave hold on, so can run this code again to generate another line
+                                                      %which will be added
+                                                      %to the same opened
+                                                      %figure.
+ 
+ xlabel({'log_{10}(h)'});
+ ylabel({'log_{10}(error)'});
+
+ 
