22a95d680523b8fe883b5655df9ad00777551156
tgolubev <tg8v3@mst.edu>
2017-03-25 14:10:04 -0400

    generalize to any binary system

--- Project3/src/project3_partb_main.cpp
+++ Project3/src/project3_partb_main.cpp
@@ -1,8 +1,8 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
-#include <cstring>
-#include <fstream>
+#include <string>
+#include <fstream>  //for files
 #include <random>
 #include <chrono>
 #include <time.h>
@@ -11,10 +11,6 @@
 
 using namespace std;
 
-
-void print_initial(int dimension, double time_step, double final_time, double *x_initial, double *v_initial, int N);
-void print_final(int dimension, double *x_final, double *v_final);
-
 int main()
 {    
     // Numerical setup
@@ -22,27 +18,26 @@ int main()
     double final_time = 50.;       // End time of calculation
     //int dimension;           // No. of spatial dimensions
 
-    //double x[3],v[3];
-
     //Set-up planets
-    planet earth(0.000003,1.,0.0,0.0,0.0,6.3,0.); // Earth: (mass,x,y,z,vx,vy,vz)
-    planet sun(1.,0.,0.,0.,0.,0.,0.);           // Sun: (mass,x,y,z,vx,vy,vz)
+    planet planet1("Earth",0.000003,1.,0.0,0.0,0.0,6.3,0.); // planet1 (name,mass,x,y,z,vx,vy,vz)   //name must be in " " marks
+    planet planet2("Sun",1.,0.,0.,0.,0.,0.,0.);           // planet2 (name,mass,x,y,z,vx,vy,vz)
 
-    //check that values from earth were actually passed to the object and can read from object. Object properties declared in planet.h
-    //and defined in planet.cpp
-    cout << "Earth Mass = " <<earth.mass<< endl;
-    cout << "Earth Initial Position = " << earth.position[0] << "," <<earth.position[1]<<","<<earth.position[2]<< endl;
+    //Output the properties of the planets
+    cout << planet1.name << "'s Mass = " <<planet1.mass<< endl;
+    cout << planet1.name <<"'s Initial Position = " << planet1.position[0] << "," <<planet1.position[1]<<","<<planet1.position[2]<< endl;
+    cout << planet2.name << "'s Mass = " <<planet2.mass<< endl;
+    cout << planet2.name <<"'s Initial Position = " << planet2.position[0] << "," <<planet2.position[1]<<","<<planet2.position[2]<< endl;
 
     //Euler method
-    ODEsolver earth_sun(0);   //create object of class ODEsolver. The 0 is just to get constructor to work.
-    earth_sun.add(sun);      //add planets to the solver
-    earth_sun.add(earth);
+    ODEsolver binary;         //create object of class ODEsolver with default constructor ODEsolver(). If put the () in declaration here, it doesn't work!
+    binary.add(planet1);      //add planets to the solver
+    binary.add(planet2);
 
     //tests
-    cout << "Gconst = " <<earth_sun.Gconst << endl;
-    cout << "Number of Planets = " <<earth_sun.total_planets<<endl;
+    cout << "Gconst = " <<binary.Gconst << endl;
+    cout << "Number of Planets = " <<binary.total_planets<<endl;
 
-    earth_sun.Euler(integration_points, final_time);  //Apply Euler's method
+    binary.Euler(integration_points, final_time);  //Apply Euler's method
 
 /*  // RK4
     solver binary_rk(5.0);
@@ -93,33 +88,4 @@ int main()
 
     return 0;
 }
-//---------------------------------------------------------------------------------------------------------------
-//Print functions
-
-void print_initial(int dimension,double time_step, double final_time,double *x_initial,double *v_initial, int N){
-    // A function that prints out the set up of the calculation
-
-    cout << "Time step = " << time_step << "; final time = " << final_time << "; integration points = " << N << endl;
-
-    cout << "Initial position = ";
-    for(int j=0;j<dimension;j++) cout << x_initial[j] << " ";
-    cout << endl;
-
-    cout << "Initial velocity = ";
-    for(int j=0;j<dimension;j++) cout << v_initial[j] << " ";
-    cout << endl;
-}
-
-void print_final(int dimension,double *x_final,double *v_final){
-    // A function that prints out the final results of the calculation
-
-    cout << "Final position = ";
-    for(int j=0; j<dimension; j++) cout << x_final[j] << " ";
-    cout << endl;
-
-    cout << "Final velocity = ";
-    for(int j=0; j<dimension; j++) cout << v_final[j] << " ";
-    cout << endl;
-}
-
 
